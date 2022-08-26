99966bbc7d35acb892924a9c741825f83039ed7c
Rakhim <rahimaliev999@gmail.com>
2022-08-26 12:45:23 +0600

    added Readme.md file

--- bomber/forms.py
+++ bomber/forms.py
@@ -55,7 +55,7 @@ class LoginForm(forms.Form):
         username = self.cleaned_data.get("username")
         password = self.cleaned_data.get("password")
 
-        return False
+        return True
 
 class AttackForm(forms.ModelForm):
     class Meta:
